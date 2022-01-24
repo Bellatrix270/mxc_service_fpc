127ae7410e7d104d72d9397f90c405f5e8c010c1
Кирил Эктоф <kirill.ektoff@yandex.ru>
2022-01-24 23:21:00 +0700

    WelcomeWindow updated

---
+++ wpf_mzc_app/WelcomeWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace wpf_mzc_app
+{
+    public partial class WelcomeWindow : Window
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
