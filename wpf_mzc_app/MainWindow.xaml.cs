548993a5f4abfe04ea859c476be6db4ca3785f10
Кирил Эктоф <kirill.ektoff@yandex.ru>
2022-01-24 22:37:10 +0700

    created project

---
+++ wpf_mzc_app/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace wpf_mzc_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
