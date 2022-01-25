5380d37a4fab547ad3f0e0dd5f08ca9ddb60820f
Кирил Эктоф <kirill.ektoff@yandex.ru>
2022-01-26 04:56:07 +0700

    MainMenuWindow added. Draft markup

---
+++ wpf_mzc_app/Views/Windows/MainMenuWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace wpf_mzc_app.Views.Windows
+{
+    public partial class MainMenuWindow : Window
+    {
+        public MainMenuWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
