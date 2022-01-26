0afe829e4adab1e9ebcbaa107fba517bf1e4c6cb
Кирил Эктоф <kirill.ektoff@yandex.ru>
2022-01-26 13:57:21 +0700

    Added binding login to MainWindow Password binding is not working yet

---
+++ wpf_mzc_app/ViewModels/AuthorizationWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace wpf_mzc_app.ViewModels
+{
+    public class AuthorizationWindowViewModel : ViewModel
+    {
+        private string _LoginUser = "Ваш логин";
+
+        public string LoginUser
+        {
+            get => _LoginUser;
+            set => Set(ref _LoginUser, value);
+        }
+
+        private string _PasswordUser = "Ваш пароль";
+
+        public string PasswordUser
+        {
+            get => _PasswordUser;
+            set => Set(ref _PasswordUser, value);
+        }
+    }
+}
